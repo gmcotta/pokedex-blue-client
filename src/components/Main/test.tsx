131ac2f35fb78ddd5e96069134cdcc1130eb88e6
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-28 17:37:53 -0300

    feat: create Main component and test

---
+++ src/components/Main/test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import Main from '.';
+
+describe('<Main />', () => {
+  it('should render the component', () => {
+    const { container } = render(<Main />);
+    expect(screen.getByRole('heading', { name: /main/i })).toBeInTheDocument();
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
