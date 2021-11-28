488537e19d135600ddb98b83b50801b7f740c62c
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-28 18:48:46 -0300

    test(Main): fix snapshot with jest-styled-components and create expectation with style

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -5,6 +5,7 @@ describe('<Main />', () => {
   it('should render the component', () => {
     const { container } = render(<Main />);
     expect(screen.getByRole('heading', { name: /main/i })).toBeInTheDocument();
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' });
     expect(container.firstChild).toMatchSnapshot();
   });
 });
