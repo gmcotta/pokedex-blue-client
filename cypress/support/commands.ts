69d5827ed1d88f5e57056a56e1b80732a78a1c4f
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-28 20:54:24 -0300

    chore: install and config @testing-library/cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,6 +23,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import '@testing-library/cypress/add-commands';
+
 Cypress.Commands.add('google', () => {
   return cy.visit('https://google.com');
 });
