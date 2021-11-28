ade7cc9b79776f83a9f1e8d166078ffa68ab22ad
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-28 20:34:08 -0300

    chore: config typescript for Cypress

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('google', () => {
+  return cy.visit('https://google.com');
+});
