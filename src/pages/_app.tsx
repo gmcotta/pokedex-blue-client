090b26f937c799b696dd36ca787cfddc6ce5aa3c
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-30 20:51:28 -0300

    feat(theme): add Rubik font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,16 @@ function App({ Component, pageProps }: AppProps) {
           name="description"
           content="A simple PokéDex using ReactJS (Next.js)"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -16,6 +16,6 @@ export default {
   },
   font: {
     family:
-      "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+      "'Rubik', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
   },
 };
