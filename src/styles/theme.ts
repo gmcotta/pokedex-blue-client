1b9dc8a2786f3851e72cae1aef9c9ddcfa33c409
Gustavo Cotta <gmcotta34@gmail.com>
2021-11-30 21:39:48 -0300

    feat(theme): add breakpoints and font settings

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -14,8 +14,28 @@ export default {
       insideWhite: 'inset -4px -4px 4px #FFFFFF',
     },
   },
+  breakpoints: {
+    sm: '375px',
+    md: '768px',
+    lg: '900px',
+    xl: '1200px',
+  },
   font: {
     family:
       "'Rubik', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    weight: {
+      light: 300,
+      normal: 400,
+      bold: 700,
+    },
+    sizes: {
+      xxs: '0.8rem',
+      xs: '1.2rem',
+      sm: '1.6rem',
+      md: '2.4rem',
+      lg: '3.2rem',
+      xl: '4.8rem',
+      xxl: '6.4rem',
+    },
   },
 };
