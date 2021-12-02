63f49a64696336a82317866db3b00f3c17f57320
Gustavo Cotta <gmcotta34@gmail.com>
2021-12-01 22:13:15 -0300

    feat(utils): add renderWithTheme helper

---
+++ src/utils/testUtils/index.tsx
@@ -0,0 +1,8 @@
+import { ReactElement } from 'react';
+import { render, RenderResult } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from '../../styles/theme';
+
+export const renderWithTheme = (ui: ReactElement): RenderResult =>
+  render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>);
