86810fe311aff356a4f2df70e2a847e408477a45
Gustavo Cotta <gmcotta34@gmail.com>
2021-12-04 13:44:41 -0300

    feat(Pokemon List): create Grid component

--- src/templates/PokemonList/index.tsx
+++ src/templates/PokemonList/index.tsx
@@ -1,6 +1,10 @@
 import * as S from './styles';
 
 const PokemonListTemplate = () => {
+  const pokemon = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30,
+  ];
   return (
     <>
       <S.Header>
@@ -12,11 +16,11 @@ const PokemonListTemplate = () => {
       </S.Header>
       <S.Container>
         <S.ListContainer>
-          <ul>
-            <li>Bulbasaur</li>
-            <li>Ivysaur</li>
-            <li>Venusaur</li>
-          </ul>
+          <S.Grid>
+            {pokemon.map((p) => (
+              <S.Item key={p}>{p}</S.Item>
+            ))}
+          </S.Grid>
           <div>Scrollbar</div>
         </S.ListContainer>
         <S.ReactIllustrationContainer>
--- src/templates/PokemonList/styles.ts
+++ src/templates/PokemonList/styles.ts
@@ -115,11 +115,25 @@ export const ListContainer = styled.section`
     z-index: 10;
     margin: 0 auto;
     padding: 2rem 1.6rem;
-    background: rgba(196, 196, 196, 0.5);
+    background-color: rgba(196, 196, 196, 0.5);
     border: 4px solid rgba(255, 255, 255, 0.3);
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25),
       inset 0px 4px 4px rgba(255, 255, 255, 0.25);
     backdrop-filter: blur(1rem);
     border-radius: 6.4rem;
+    display: flex;
   `}
+`;
+
+export const Grid = styled.ul`
+  width: 97%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+  overflow-y: auto;
+`;
+
+export const Item = styled.li`
+  width: 100%;
+  aspect-ratio: 1;
 `;
