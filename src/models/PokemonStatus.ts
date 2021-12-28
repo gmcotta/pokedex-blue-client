bcc92745aaa799e96073f500f70bdf48050900b1
Gustavo Cotta <gmcotta34@gmail.com>
2021-12-28 11:49:34 -0300

    feat: create PokemonStatus model

---
+++ src/models/PokemonStatus.ts
@@ -0,0 +1,7 @@
+export type PokemonStatus =
+  | 'hp'
+  | 'attack'
+  | 'defense'
+  | 'speed'
+  | 'specialAttack'
+  | 'specialDefense';
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,2 +1,3 @@
 export * from './PokemonGridItem';
 export * from './PokemonType';
+export * from './PokemonStatus';
