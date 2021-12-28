d6e0f39346552e0d15232e387b3aec00dfe2f855
Gustavo Cotta <gmcotta34@gmail.com>
2021-12-28 16:44:03 -0300

    feat(pokemon-details): make logic to change tabs and redirect to pokemon

--- src/templates/PokemonDetails/index.tsx
+++ src/templates/PokemonDetails/index.tsx
@@ -1,12 +1,15 @@
-// import Chip from '@/components/Chip';
+import { useEffect, useState } from 'react';
+
+import Chip from '@/components/Chip';
 import PokemonGridItem from '@/components/PokemonGridItem';
-// import StatusBar from '@/components/StatusBar';
+import StatusBar from '@/components/StatusBar';
 import { PokemonEvolutionStage, PokemonInfo } from '@/models/PokemonInfo';
-// import formatPokemonHeight from '@/utils/formatPokemonHeight';
+import formatPokemonHeight from '@/utils/formatPokemonHeight';
 import formatPokemonName from '@/utils/formatPokemonName';
-// import formatPokemonWeight from '@/utils/formatPokemonWeight';
+import formatPokemonWeight from '@/utils/formatPokemonWeight';
 
 import * as S from './styles';
+import { useRouter } from 'next/router';
 
 type PokemonDetailsTemplateProps = {
   details: PokemonInfo;
@@ -20,7 +23,16 @@ type EvolutionChain = {
 
 type StageNames = 'firstStage' | 'secondStage' | 'thirdStage';
 
+type TabOptions = 'info' | 'status' | 'evolution';
+
 const PokemonDetailsTemplate = ({ details }: PokemonDetailsTemplateProps) => {
+  const [activeTab, setActiveTab] = useState<TabOptions>('info');
+  const { push } = useRouter();
+
+  useEffect(() => {
+    setActiveTab('info');
+  }, [push]);
+
   function countEvolutionStages(evolution: EvolutionChain) {
     let count = 0;
     const stages = Object.keys(evolution);
@@ -30,126 +42,150 @@ const PokemonDetailsTemplate = ({ details }: PokemonDetailsTemplateProps) => {
     return count;
   }
 
+  const handleGridItemClick = (id: number) => {
+    console.log('click');
+    push(`/pokemon/${id}`);
+  };
+
   return (
     <S.Wrapper>
       <S.Header>
         <div>
-          <button type="button">Info</button>
-          <button type="button">Status</button>
-          <button type="button">Evolution</button>
+          <button onClick={() => setActiveTab('info')} type="button">
+            Info
+          </button>
+          <button onClick={() => setActiveTab('status')} type="button">
+            Status
+          </button>
+          <button onClick={() => setActiveTab('evolution')} type="button">
+            Evolution
+          </button>
         </div>
       </S.Header>
       <S.Container>
         <S.PokemonName>
           {formatPokemonName(details.pokemonId!, details.name!)}
         </S.PokemonName>
-        <S.EvolutionGrid
-          numberOfStages={countEvolutionStages(details.evolutionChain)}
-        >
-          {details.evolutionChain.firstStage!.length > 0 && (
-            <S.StageContainer>
-              <h2>First Stage</h2>
-              <S.StageGrid>
-                {details.evolutionChain.firstStage!.map((pokemon) => (
-                  <PokemonGridItem
-                    key={pokemon.pokemonId!}
-                    imgSrc={pokemon.frontImageUrl!}
-                    name={pokemon.name!}
-                    pokemonId={pokemon.pokemonId!}
-                    pokemonTypes={pokemon.pokemonTypes!}
-                  />
-                ))}
-              </S.StageGrid>
-            </S.StageContainer>
-          )}
-          {details.evolutionChain.secondStage!.length > 0 && (
-            <S.StageContainer>
-              <h2>Second Stage</h2>
-              <S.StageGrid>
-                {details.evolutionChain.secondStage!.map((pokemon) => (
-                  <PokemonGridItem
-                    key={pokemon.pokemonId!}
-                    imgSrc={pokemon.frontImageUrl!}
-                    name={pokemon.name!}
-                    pokemonId={pokemon.pokemonId!}
-                    pokemonTypes={pokemon.pokemonTypes!}
-                  />
-                ))}
-              </S.StageGrid>
-            </S.StageContainer>
-          )}
-          {details.evolutionChain.thirdStage!.length > 0 && (
-            <S.StageContainer>
-              <h2>Third Stage</h2>
-              <S.StageGrid>
-                {details.evolutionChain.thirdStage!.map((pokemon) => (
-                  <PokemonGridItem
-                    key={pokemon.pokemonId!}
-                    imgSrc={pokemon.frontImageUrl!}
-                    name={pokemon.name!}
-                    pokemonId={pokemon.pokemonId!}
-                    pokemonTypes={pokemon.pokemonTypes!}
-                  />
-                ))}
-              </S.StageGrid>
-            </S.StageContainer>
-          )}
-        </S.EvolutionGrid>
-        {/* <S.InfoGrid>
-          <S.ImageCard>
-            <img src={`${details.officialImageUrl}`} />
-          </S.ImageCard>
-          <S.InfoCard>
-            <h2>{details.genus}</h2>
-            <div>
-              <div>
-                <p>Type</p>
-                <div>
-                  {details.pokemonTypes?.map((type) => (
-                    <Chip key={type} name={type!} hasImage />
+        {activeTab === 'evolution' && (
+          <S.EvolutionGrid
+            numberOfStages={countEvolutionStages(details.evolutionChain)}
+          >
+            {details.evolutionChain.firstStage!.length > 0 && (
+              <S.StageContainer>
+                <h2>First Stage</h2>
+                <S.StageGrid>
+                  {details.evolutionChain.firstStage!.map((pokemon) => (
+                    <PokemonGridItem
+                      key={pokemon.pokemonId!}
+                      imgSrc={pokemon.frontImageUrl!}
+                      name={pokemon.name!}
+                      pokemonId={pokemon.pokemonId!}
+                      pokemonTypes={pokemon.pokemonTypes!}
+                      onClick={() => handleGridItemClick(pokemon.pokemonId!)}
+                    />
                   ))}
+                </S.StageGrid>
+              </S.StageContainer>
+            )}
+            {details.evolutionChain.secondStage!.length > 0 && (
+              <S.StageContainer>
+                <h2>Second Stage</h2>
+                <S.StageGrid>
+                  {details.evolutionChain.secondStage!.map((pokemon) => (
+                    <PokemonGridItem
+                      key={pokemon.pokemonId!}
+                      imgSrc={pokemon.frontImageUrl!}
+                      name={pokemon.name!}
+                      pokemonId={pokemon.pokemonId!}
+                      pokemonTypes={pokemon.pokemonTypes!}
+                      onClick={() => handleGridItemClick(pokemon.pokemonId!)}
+                    />
+                  ))}
+                </S.StageGrid>
+              </S.StageContainer>
+            )}
+            {details.evolutionChain.thirdStage!.length > 0 && (
+              <S.StageContainer>
+                <h2>Third Stage</h2>
+                <S.StageGrid>
+                  {details.evolutionChain.thirdStage!.map((pokemon) => (
+                    <PokemonGridItem
+                      key={pokemon.pokemonId!}
+                      imgSrc={pokemon.frontImageUrl!}
+                      name={pokemon.name!}
+                      pokemonId={pokemon.pokemonId!}
+                      pokemonTypes={pokemon.pokemonTypes!}
+                      onClick={() => handleGridItemClick(pokemon.pokemonId!)}
+                    />
+                  ))}
+                </S.StageGrid>
+              </S.StageContainer>
+            )}
+          </S.EvolutionGrid>
+        )}
+        {activeTab !== 'evolution' && (
+          <S.InfoGrid>
+            <S.ImageCard>
+              <img src={`${details.officialImageUrl}`} />
+            </S.ImageCard>
+            {activeTab === 'info' && (
+              <S.InfoCard>
+                <h2>{details.genus}</h2>
+                <div>
+                  <div>
+                    <p>Type</p>
+                    <div>
+                      {details.pokemonTypes?.map((type) => (
+                        <Chip key={type} name={type!} hasImage />
+                      ))}
+                    </div>
+                  </div>
+                  <div>
+                    <p>Height</p>
+                    <p>{formatPokemonHeight(details.height!)}</p>
+                  </div>
+                  <div>
+                    <p>Weight</p>
+                    <p>{formatPokemonWeight(details.weight!)}</p>
+                  </div>
                 </div>
-              </div>
-              <div>
-                <p>Height</p>
-                <p>{formatPokemonHeight(details.height!)}</p>
-              </div>
-              <div>
-                <p>Weight</p>
-                <p>{formatPokemonWeight(details.weight!)}</p>
-              </div>
-            </div>
-            <p>{details.description}</p>
-          </S.InfoCard>
-          <S.StatusCard>
-            <StatusBar status="hp" name="HP" value={details.status!.hp!} />
-            <StatusBar
-              status="attack"
-              name="Attack"
-              value={details.status!.attack!}
-            />
-            <StatusBar
-              status="defense"
-              name="Defense"
-              value={details.status!.defense!}
-            />
-            <StatusBar
-              status="speed"
-              name="Speed"
-              value={details.status!.speed!}
-            />
-            <StatusBar
-              status="specialAttack"
-              name="Special Attack"
-              value={details.status!.specialAttack!}
-            />
-            <StatusBar
-              status="specialDefense"
-              name="Special Defense"
-              value={details.status!.specialDefense!}
-            />
-          </S.StatusCard>
-        </S.InfoGrid> */}
+                <p>{details.description}</p>
+              </S.InfoCard>
+            )}
+            {activeTab === 'status' && (
+              <S.StatusCard>
+                <StatusBar status="hp" name="HP" value={details.status!.hp!} />
+                <StatusBar
+                  status="attack"
+                  name="Attack"
+                  value={details.status!.attack!}
+                />
+                <StatusBar
+                  status="defense"
+                  name="Defense"
+                  value={details.status!.defense!}
+                />
+                <StatusBar
+                  status="speed"
+                  name="Speed"
+                  value={details.status!.speed!}
+                />
+                <StatusBar
+                  status="specialAttack"
+                  name="Special Attack"
+                  value={details.status!.specialAttack!}
+                />
+                <StatusBar
+                  status="specialDefense"
+                  name="Special Defense"
+                  value={details.status!.specialDefense!}
+                />
+              </S.StatusCard>
+            )}
+          </S.InfoGrid>
+        )}
+
+        {/*  */}
       </S.Container>
     </S.Wrapper>
   );
